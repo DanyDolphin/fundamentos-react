57a46bcdb168050d1f4843731b9d2406ea166fda
DanielCastillo59814 <daniel.castillo@payjoy.com>
2023-10-23 20:58:42 -0600

    manejadno estados

--- src/layouts/NewExpense.jsx
+++ src/layouts/NewExpense.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 function NewExpense(props) {
+    const [descripcion, setDescripcion] = useState()
+    const [monto, setMonto] = useState()
+    const [fecha, setFecha] = useState()
+
     function handleDescripcionChange(event) {
-        console.log(event.target.value)
+        setDescripcion(event.target.value)
+    }
+
+    function handleMontoChange(event) {
+        setMonto(event.target.value)
+    }
+
+    function handleFechaChange(event) {
+        setFecha(event.target.value)
+    }
+
+    function handleClick() {
+        console.log(`
+        Descripcion: ${descripcion}
+        Monto: ${monto}
+        Fecha: ${fecha.toString()}
+        `);
     }
 
     return (
@@ -12,10 +32,19 @@ function NewExpense(props) {
             placeholder="Descripcion"
             onChange={handleDescripcionChange}/>
         <br />
-        <input type="text" placeholder="Monto" />
+        <input
+            type="text"
+            placeholder="Monto"
+            onChange={handleMontoChange}/>
         <br />
-        <input type="date" placeholder="Fecha" />
-        <button>Agregar</button>
+        <input
+            type="date"
+            placeholder="Fecha"
+            onChange={handleFechaChange}/>
+        <button
+            onClick={handleClick}>
+            Agregar
+        </button>
         </>
     );
 }
