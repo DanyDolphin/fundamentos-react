5c40eafc03239d310881af81d18ff7d42092974e
DanielCastillo59814 <daniel.castillo@payjoy.com>
2023-11-06 21:32:51 -0600

    haciendo fetch en useEffect

--- Proyecto-Inicial/src/components/Home/Home.js
+++ Proyecto-Inicial/src/components/Home/Home.js
@@ -1,15 +1,61 @@
-import {useContext} from 'react'
+import {useContext, useEffect, useState} from 'react'
 import Card from "../UI/Card/Card";
 import styles from "./Home.module.css";
 
 import AuthContext from "../../contexts/AuthContext";
 
 function Home() {
+  const [user, setUser] = useState({})
+  const [error, setError] = useState()
+  const [isLoading, setIsLoading] = useState(true);
+
   const authContext = useContext(AuthContext)
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      setIsLoading(true);
+      const id = localStorage.getItem('userId');
+      const url = `https://react-bedu-default-rtdb.firebaseio.com/users/${id}.json`
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        setError('Error al obtener datos del usuario.');
+        return
+      }
+
+      const responseData = await response.json();
+      setUser(responseData);
+      setIsLoading(false);
+    }
+
+    fetchUser();
+  }, []);
+
   return (
     <Card className={styles.home}>
-      <h1>¡Bienvenido!</h1>
-      <p>Inicie sesion? {authContext.isLoggedIn ? 'si' : 'no'}</p>
+      {isLoading ? (
+        <h1>Cargando...</h1>
+      ) : error ? (
+        <h1>{error}</h1>
+      ) : <>
+        <h1>¡Bienvenido!</h1>
+        <table>
+          <tbody>
+          <tr>
+            <td>Nombre</td>
+            <td>{user.first_name}</td>
+          </tr>
+          <tr>
+            <td>Apellido</td>
+            <td>{user.last_name}</td>
+          </tr>
+          <tr>
+            <td>Email</td>
+            <td>{user.email}</td>
+          </tr>
+          </tbody>
+        </table>
+      </>}
     </Card>
   );
 }
